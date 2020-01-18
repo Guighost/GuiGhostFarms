e3631dc960398ed1fd4913411d1f2270c2fa6837
Duane E Burnett <info@guighost.com>
2020-01-18 16:10:10 -0500

    more ad work

--- platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -7,10 +7,22 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "clobbers": [
         "Enhance"
       ]
+    },
+    {
+      "id": "cordova-plugin-facebookads.FacebookAds",
+      "file": "plugins/cordova-plugin-facebookads/www/FacebookAds.js",
+      "pluginId": "cordova-plugin-facebookads",
+      "clobbers": [
+        "window.FacebookAds"
+      ]
     }
   ];
   module.exports.metadata = {
     "cordova-plugin-whitelist": "1.3.4",
-    "cordova-plugin-enhance": "3.0.0"
+    "cordova-plugin-enhance": "3.0.0",
+    "cordova-facebook-audience-network": "4.22.1",
+    "cordova-plugin-extension": "1.5.4",
+    "cordova-facebook-audnet-sdk": "4.26.2",
+    "cordova-plugin-facebookads": "4.23.2"
   };
 });
