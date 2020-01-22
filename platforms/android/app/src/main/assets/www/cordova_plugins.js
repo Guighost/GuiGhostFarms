660735e4bb98e94dffd98a4e39cd0a6dadd4ad39
Duane E Burnett <info@guighost.com>
2020-01-21 21:15:52 -0500

    plugins

--- platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -24,6 +24,14 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
         "cordova.InAppBrowser.open",
         "window.open"
       ]
+    },
+    {
+      "id": "cordova-plugin-market.Market",
+      "file": "plugins/cordova-plugin-market/www/market.js",
+      "pluginId": "cordova-plugin-market",
+      "clobbers": [
+        "cordova.plugins.market"
+      ]
     }
   ];
   module.exports.metadata = {
@@ -31,6 +39,7 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     "cordova-plugin-fbanfree": "0.0.8",
     "cordova-custom-config": "5.1.0",
     "cordova-appodeal-prime": "1.0.1",
-    "cordova-plugin-inappbrowser": "3.2.0"
+    "cordova-plugin-inappbrowser": "3.2.0",
+    "cordova-plugin-market": "1.2.0"
   };
 });
