c3c913ccdaa329896833adf144a4cd92f6f05240
Duane E Burnett <info@guighost.com>
2020-01-22 08:24:26 -0500

    plugin config

--- www/js/index.js
+++ www/js/index.js
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
- requires('cordova')
+
 var app = {
     // Application Constructor
     initialize: function() {
