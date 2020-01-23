1bafe86f60d607bd5b1f7aee10a7ad1cf70ee9e6
Duane E Burnett <info@guighost.com>
2020-01-22 21:19:38 -0500

    Revert "another config update"
    This reverts commit 1d8b542f7fdd304b9e03a8276681448f9643f351.

--- www/js/index.js
+++ www/js/index.js
@@ -1,22 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
 var app = {
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
@@ -37,12 +18,37 @@ var app = {
                 e.preventDefault();
                 window.open(''.concat(this.href), '_blank');
             }
-
-
         }
     }
+    document.getElementById("openBrowser").addEventListener("click", openBrowser('https://play.google.com/store/apps/details?id=com.wGuiGhostFarms_9861566'));
+  document.getElementById("logo").addEventListener("click", openBrowser('http://checanada.ca'));
+function openBrowser(url) {
 
+   var target = '_blank';
+   var options = "location = yes"
+   var ref = cordova.InAppBrowser.open(url, target, options);
+
+   ref.addEventListener('loadstart', loadstartCallback);
+   ref.addEventListener('loadstop', loadstopCallback);
+   ref.addEventListener('loaderror', loaderrorCallback);
+   ref.addEventListener('exit', exitCallback);
+
+   function loadstartCallback(event) {
+      console.log('Loading started: '  + event.url)
+   }
+
+   function loadstopCallback(event) {
+      console.log('Loading finished: ' + event.url)
+   }
+
+   function loaderrorCallback(error) {
+      console.log('Loading error: ' + error.message)
+   }
+
+   function exitCallback() {
+      console.log('Browser is closed...')
+   }
+}
 };
 
 app.initialize();
-
