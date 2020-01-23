573d692def52f7241404ca555a16ed177e2020fe
Duane E Burnett <info@guighost.com>
2020-01-22 20:38:02 -0500

    inappbrowser

--- www/js/index.js
+++ www/js/index.js
@@ -20,6 +20,35 @@ var app = {
             }
         }
     }
+    document.getElementById("openBrowser").addEventListener("click", openBrowser('https://play.google.com/store/apps/details?id=com.wGuiGhostFarms_9861566'));
+  document.getElementById("logo").addEventListener("click", openBrowser('http://checanada.ca'));
+function openBrowser(url) {
+
+   var target = '_blank';
+   var options = "location = yes"
+   var ref = cordova.InAppBrowser.open(url, target, options);
+
+   ref.addEventListener('loadstart', loadstartCallback);
+   ref.addEventListener('loadstop', loadstopCallback);
+   ref.addEventListener('loaderror', loaderrorCallback);
+   ref.addEventListener('exit', exitCallback);
+
+   function loadstartCallback(event) {
+      console.log('Loading started: '  + event.url)
+   }
+
+   function loadstopCallback(event) {
+      console.log('Loading finished: ' + event.url)
+   }
+
+   function loaderrorCallback(error) {
+      console.log('Loading error: ' + error.message)
+   }
+
+   function exitCallback() {
+      console.log('Browser is closed...')
+   }
+}
 };
 
 app.initialize();
