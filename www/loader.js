bd43fb20723f4245df2ece4641f0188b18c99d33
Duane E Burnett <info@guighost.com>
2021-09-03 15:01:08 -0400

    commit before gradle upgrade

--- www/loader.js
+++ www/loader.js
@@ -1,7 +1,24 @@
 /*jshint esversion: 6 */
-window.addEventListener('load', (event) => {             
+// Add the Firebase products that you want to use
+
+// Add the Firebase services that you want to use
+
+window.addEventListener('load', (event) => { 
+  let loadTextStart = document.getElementById('loadingText1');
+  let startLogo = document.getElementById('startLogo');
+  setTimeout(function(){    loadTextStart.innerText = 'Getting Images...'; startLogo.style.scale = '1.1'; }, 1250); 
+  
+  setTimeout(function(){
+    loadTextStart.innerText = 'Loading Data...';  startLogo.style.scale = '1.0'; 
     try{ farming.start();  }
-    catch(err){ alert("Error Loading ---   " + err ); console.log(event);    }
-         
-   
+    catch(err){ alert("Error Loadingsas ---   " + err ); console.log(err);    }
+
+  }, 2000);        
+       
+  setTimeout(function(){    loadTextStart.innerText = 'Building World...'; startLogo.style.scale = '1.1';  }, 2750);  
+  setTimeout(function(){    loadTextStart.innerText = 'Loading Crops...'; startLogo.style.scale = '1.0';  }, 3500);    
+
+  
+
+
   });
